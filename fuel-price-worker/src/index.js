59fc68c40657e566487683a12102d07976ea2e62
Cailan White <me@cailanwhite.dev>
2026-06-16 20:36:33 +0100

    remove token caching

--- fuel-price-worker/src/index.js
+++ fuel-price-worker/src/index.js
@@ -3,42 +3,41 @@
 export default {
     async scheduled(event, env, ctx) {
         await updatePrice(env);
+    }, async fetch(event, env, ctx) {
+        await updatePrice(env);
+        return new Response("OK");
     }
 };
 
 // update fuel price
 async function updatePrice(env) {
-    let token = await env.FUEL_CACHE.get("oauth-token");
-
+    let token = undefined;
+    
     // fetch new token
-    if (!token) {
-        try {
-            const tokenRes = await fetch ("https://www.fuel-finder.service.gov.uk/api/v1/oauth/generate_access_token", {
-                method: "POST",
-                headers: { 
-                    "Content-Type": "application/x-www-form-urlencoded", 
-                    "Accept": "application/json", 
-                    "User-Agent": "Mozilla/5.0 (compatible; FuelPriceWorker/1.0)"
-                }, 
-                body: new URLSearchParams ({
-                    grant_type: "client_credentials", 
-                    client_id: env.FUEL_CLIENT_ID, 
-                    client_secret: env.FUEL_CLIENT_SECRET,
-                    scope: "fuelfinder.read"
-                })
-            });
+    try {
+        const tokenRes = await fetch ("https://www.fuel-finder.service.gov.uk/api/v1/oauth/generate_access_token", {
+            method: "POST",
+            headers: { 
+                "Content-Type": "application/x-www-form-urlencoded", 
+                "Accept": "application/json", 
+                "User-Agent": "Mozilla/5.0 (compatible; FuelPriceWorker/1.0)"
+            }, 
+            body: new URLSearchParams ({
+                grant_type: "client_credentials", 
+                client_id: env.FUEL_CLIENT_ID, 
+                client_secret: env.FUEL_CLIENT_SECRET,
+                scope: "fuelfinder.read"
+            })
+        });
 
-            // update kv db cache with token
-            const tokenData = await tokenRes.json();
-            token = tokenData.data.access_token;
-            await env.FUEL_CACHE.put("oauth-token", token, {
-                expirationTtl: tokenData.data.expires_in - 60
-            });
-        } catch (err) {
-            console.error("Error getting token", err);
-            return;
-        }
+        // update kv db cache with token
+        const tokenData = await tokenRes.json();
+        token = tokenData.data.access_token;
+    } catch (err) {
+        console.error("Error getting token", err);
+        return;
     }
+    
 
     // fetch price data
     try {
@@ -66,4 +65,6 @@ async function updatePrice(env) {
         console.error("Fuel Price fetch error", err);
         return;
     }
+
+    console.log(await env.FUEL_CACHE.get("station-price"))
 }
