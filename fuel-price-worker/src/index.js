73b6a7e102fc96c608e34ad11bb7106c9bfc1090
Cailan White <me@cailanwhite.dev>
2026-06-16 18:21:18 +0100

    refractor worker to try/catch model

--- fuel-price-worker/src/index.js
+++ fuel-price-worker/src/index.js
@@ -12,56 +12,51 @@ async function updatePrice(env) {
 
     // fetch new token
     if (!token) {
-        const tokenRes = await fetch ("https://www.fuel-finder.service.gov.uk/api/v1/oauth/generate_access_token", {
-            method: "POST",
-            headers: { 
-                "Content-Type": "application/x-www-form-urlencoded", 
-                "Accept": "application/json", 
-                "User-Agent": "Mozilla/5.0 (compatible; FuelPriceWorker/1.0)"
-             }, 
-            body: new URLSearchParams ({
-                grant_type: "client_credentials", 
-                client_id: env.FUEL_CLIENT_ID, 
-                client_secret: env.FUEL_CLIENT_SECRET,
-                scope: "fuelfinder.read"
-            })
-        });
+        try {
+            const tokenRes = await fetch ("https://www.fuel-finder.service.gov.uk/api/v1/oauth/generate_access_token", {
+                method: "POST",
+                headers: { 
+                    "Content-Type": "application/x-www-form-urlencoded", 
+                    "Accept": "application/json", 
+                    "User-Agent": "Mozilla/5.0 (compatible; FuelPriceWorker/1.0)"
+                }, 
+                body: new URLSearchParams ({
+                    grant_type: "client_credentials", 
+                    client_id: env.FUEL_CLIENT_ID, 
+                    client_secret: env.FUEL_CLIENT_SECRET,
+                    scope: "fuelfinder.read"
+                })
+            });
 
-        console.log(tokenRes)
-
-        // update kv db cache with token
-        const tokenData = await tokenRes.json();
-        token = tokenData.data.access_token;
-        await env.FUEL_CACHE.put("oauth_token", token, {
-            expirationTtl: tokenData.data.expires_in - 60
-        });
+            // update kv db cache with token
+            const tokenData = await tokenRes.json();
+            token = tokenData.data.access_token;
+            await env.FUEL_CACHE.put("oauth_token", token, {
+                expirationTtl: tokenData.data.expires_in - 60
+            });
+        } catch (err) {
+            console.error("Error getting token", tokenRes.status);
+            return;
+        }
     }
 
     // fetch price data
-    const lastCheck = await env.FUEL_CACHE.get("last-price-check");
-    const url = `https://www.fuel-finder.service.gov.uk/api/v1/pfs/fuel-prices?batch-number=10`;
-    const dataRes = await fetch(url, {
-        headers: { 
-            Authorization: `Bearer ${token}`,
-            "Accept": "application/json", 
-            "User-Agent": "Mozilla/5.0 (compatible; FuelPriceWorker/1.0)"
-        }
-    });
-    if (!dataRes.ok) {
-        console.error("fuel price api error", dataRes.status);
+    try {
+        const dataRes = await fetch("https://www.fuel-finder.service.gov.uk/api/v1/pfs/fuel-prices?batch-number=10", {
+            headers: { 
+                Authorization: `Bearer ${token}`,
+                "Accept": "application/json", 
+                "User-Agent": "Mozilla/5.0 (compatible; FuelPriceWorker/1.0)"
+            }
+        });
+
+        // manipulate response data, and store in kv cache
+        const data = await dataRes.json();
+        const ourPrice = data.find(s => s.node_id === env.STATION_ID);
+        await env.FUEL_CACHE.put("station-price", JSON.stringify(ourPrice));
+    } catch (err) {
+        console.error("Fuel Price fetch error", dataRes.status);
         return;
     }
-
-    // manipulate response data, and store in kv cache
-    const data = await dataRes.json();
-    const ourPrice = data.find(s => s.node_id === env.STATION_ID);
-    await env.FUEL_CACHE.put("station-price", JSON.stringify(ourPrice));
-
-    // formatting time to match what is expected 
-    function formatTime(date) {
-        const pad = n => String(n).padStart(2, "0");
-        return `${date.getUTCFullYear()}-${pad(date.getUTCMonth() + 1)}-${pad(date.getUTCDate())} ${pad(date.getUTCHours())}:${pad(date.getUTCMinutes())}:${pad(date.getUTCSeconds())}`;
-    }
-    const now = formatTime(new Date());
-    await env.FUEL_CACHE.put("last-price-check", now);
+    console.log(await env.FUEL_CACHE.get("station-price"))
 }
