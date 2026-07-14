1cf960ae5a59f40e01c49867a06086fa2e5c4ef4
Cailan White <me@cailanwhite.dev>
2026-07-14 13:30:36 +0100

    update map to load on click instead, added preview image

--- pages/site.js
+++ pages/site.js
@@ -49,5 +49,24 @@ async function getEVAvailabilityTarriff() {
   }
 }
 
+// load interactive map in place of static preview 
+function loadInteractiveMap() {
+  const previewMap = document.getElementById("preview-map");
+  previewMap.outerHTML = `<iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2144.534675157363!2d-3.186799721988205!3d57.48626057377476!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4885a44b313b653f%3A0x993479a922a81bb3!2sCraigellachie%20Filling%20Station%20Ltd!5e0!3m2!1sen!2suk!4v1781563003130!5m2!1sen!2suk"
+                  allowfullscreen=""
+                  loading="lazy"
+                  referrerpolicy="no-referrer-when-downgrade"
+                ></iframe>`
+  localStorage.setItem("mapConsent", "true")
+}
+
+// check if user previously loaded interactive map
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("mapConsent") === "true") {
+    loadInteractiveMap();
+  }
+});
+
 getFuelPrice();
 getEVAvailabilityTarriff();
