a79d5603f1ef3b06baf218ee260da522b2e16922
Cailan White <me@cailanwhite.dev>
2026-07-18 13:44:17 +0100

    map js, update to readme

---
+++ pages/assets/js/map.js
@@ -0,0 +1,33 @@
+// displaying the interactive map
+
+import { layers } from "https://unpkg.com/protomaps-themes-base@2/dist/index.js";
+const pinLocation = [-4.21, 57.47];
+
+// fetches the basemap from our domain, and then styles it
+const map = new maplibregl.Map({
+  container: "map",
+  style: {
+    version: 8,
+    glyphs:
+      "https://protomaps.github.io/basemaps-assets/fonts/{fontstack}/{range}.pbf",
+    sprite: "https://protomaps.github.io/basemaps-assets/sprites/v4/light",
+    sources: {
+      basemap: {
+        type: "vector",
+        url: "https://map-tile.craigellachiefillingstation.co.uk/scotland/map.json",
+      },
+    },
+    layers: layers("basemap", "light", { lang: "en" }),
+  },
+  center: pinLocation,
+  zoom: 10,
+});
+
+map.addControl(new maplibregl.NavigationControl());
+
+new maplibregl.Marker()
+  .setLngLat(pinLocation)
+  .setPopup(
+    new maplibregl.Popup().setHTML("<b>Craigellachie Filling Station</b>"),
+  )
+  .addTo(map);
