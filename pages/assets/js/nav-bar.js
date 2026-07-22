cdaa08b2025b9f712ffa0ea2f1ce1834641dd826
Cailan White <me@cailanwhite.dev>
2026-07-22 18:46:28 +0100

    updated header, mobile scaling and navbar

--- pages/assets/js/nav-bar.js
+++ pages/assets/js/nav-bar.js
@@ -3,9 +3,15 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 const navbarDivider = document.getElementsByClassName("navbar-divier")[0];
+const aboutLink = document.getElementsByClassName("about")[0];
 
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
   toggleButton.classList.toggle("active");
   navbarDivider.classList.toggle("active");
+});
+
+aboutLink.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+  toggleButton.classList.toggle("active");
 });
