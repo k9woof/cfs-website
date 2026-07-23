24602cdde913f7dd5131e2ad4758f854423d6d5c
Cailan White <me@cailanwhite.dev>
2026-07-23 19:51:18 +0100

    update logo font, update map info

--- pages/assets/js/map.js
+++ pages/assets/js/map.js
@@ -24,12 +24,14 @@ const map = new maplibregl.Map({
   center: pinLocation,
   zoom: 11,
 });
+
+// nav/marker
 map.addControl(new maplibregl.NavigationControl());
 new maplibregl.Marker()
   .setLngLat(pinLocation)
   .setPopup(
     new maplibregl.Popup().setHTML(
-      "<b>Craigellachie Filling Station</b><br><p>Hill Street, Craigellachie,<br> Moray, AB38 9TB</p>",
+      '<b>Craigellachie Filling Station</b><br><p>Hill Street, Craigellachie,<br> Moray, AB38 9TB</p><a href="https://www.google.com/maps/dir//Craigellachie+Filling+Station+Ltd,+Hill+St,+Craigellachie,+Aberlour+AB38+9TB/@57.4862635,-3.1867997,17z/data=!4m17!1m7!3m6!1s0x4885a44b313b653f:0x993479a922a81bb3!2sCraigellachie+Filling+Station+Ltd!8m2!3d57.4862606!4d-3.1842248!16s%2Fg%2F1tf3b352!4m8!1m0!1m5!1m1!1s0x4885a44b313b653f:0x993479a922a81bb3!2m2!1d-3.1842248!2d57.4862606!3e0?entry=ttu&g_ep=EgoyMDI2MDcwOC4wIKXMDSoASAFQAw%3D%3D" rel="noopener" target="_blank" title="Get Directions for Craigellachie Filling Station on Google Maps">Get Directions</a>',
     ),
   )
   .addTo(map);
